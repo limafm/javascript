5e88a07448c04c4bba20fe9182c10edf1d162e5f
Fábio Lima <lr.fabioml@gmail.com>
2018-08-24 19:36:34 -0400

    destructuring on objects
    Some examples of destructuring on objects

---
+++ destructuringObject.js
@@ -0,0 +1,34 @@
+// Some destructuring examples
+// Available since ES6
+
+// Original Object
+const myObj = {
+  level11: {
+    'level2-1': 'value11',
+    'level2-2': 'value12',
+  },
+  level12: {
+    'level2-1': 'value21',
+  }
+};
+
+/* Use destructuring to get level1 properties */
+const { level11, level12 } = myObj;
+
+console.log(level11);
+//O: { 'level2-1': 'value11', 'level2-2': 'value12' }
+console.log(level12);
+//O: { 'level2-1': 'value21' }
+
+/* Use destructuring storing the data on "aux" */
+
+const { level11: aux } = myObj;
+
+console.log(aux);
+//O: { 'level2-1': 'value11', 'level2-2': 'value12' }
+
+/* Set Default Values */
+const { level11111: aux1 = 'default value :D' } = myObj;
+
+console.log(aux1);
+//O: default value :D
